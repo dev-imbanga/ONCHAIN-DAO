0fa40ebbdf2f222e17feb442b55fc0e82e45ce3c
Imbanga <blackthorn101@outlook.com>
2023-09-20 07:05:21 +0300

    changes

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,32 +1,47 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+const hre = require("hardhat")
+async function sleep(ms){
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+async function main(){
+
+  const nftContract = await hre.ethers.deployContract("CryptoDevsNFT");
+  await nftContract.waitForDeployment();
+  console.log("CryptoDevsNFT deployed to:", nftContract.target);
+
+  const fakeNftMarketplaceContract = await hre.ethers.deployContract("FakeNFTMarketplace");
+  await fakeNftMarketplaceContract.waitForDeployment();
+  console.log( "FakeNFTMarketplace deployed to:", fakeNftMarketplaceContract.target);
+
+  const amount = hre.ethers.parseEther("1");
+  const daoContract = await hre.ethers.deployContract("CryptoDevsDAO", [fakeNftMarketplaceContract.target, nftContract.target,], {value: amount,});
+  await daoContract.waitForDeployment();
+  console.log("CryptoDevsDAO deployed to:", daoContract.target);
+
+   await hre.run("verify:verify", {
+    address: nftContract.target,
+    constructorArguments: [],
+  });
+
+   await hre.run("verify:verify", {
+    address: fakeNftMarketplaceContract.target,
+    constructorArguments: [],
+  });
+
+  await hre.run("verify:verify", {
+    address: daoContract.target,
+    constructorArguments: [
+      fakeNftMarketplaceContract.target,
+      nftContract.target,
+    ],
+  });
+
+
+}
+
+
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
